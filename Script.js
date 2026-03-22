a86426966898514517976bd678aca40bc1619a95
vhumi2004-CSE <vhumi2004@gmail.com>
2026-03-22 20:17:58 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,49 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+  if(inputBox.value.trim() === ''){
+    alert("You must write something!");
+  }
+  else{
+    let li = document.createElement("li");
+    li.textContent = inputBox.value;
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+
+    li.appendChild(span);
+    listContainer.appendChild(li);
+
+    inputBox.value = "";
+
+    saveData();
+  }
+}
+
+listContainer.addEventListener("click", function(e){
+
+  if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+  } 
+  else if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData();
+  }
+
+});
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+  let data = localStorage.getItem("data");
+
+  if(data){
+    listContainer.innerHTML = data;
+  }
+}
+
+showTask();
